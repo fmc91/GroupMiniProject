21b57e153b4a91e341836c2a6b6b1de45f39f79b
Vinay Patel <vinay.light@hotmail.co.uk>
2020-10-28 12:37:56 +0000

    addingClasses

---
+++ CovidTrackingApp/CovidTrackingApp/Data/Booking.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CovidTrackingApp.Data
+{
+    public partial class Booking
+    {
+        public int BookingId { get; set; }
+        public int UserId { get; set; }
+        public int VenueId { get; set; }
+        public TimeSpan ArrivalTime { get; set; }
+        public TimeSpan DepartureTime { get; set; }
+        public DateTime BookingDate { get; set; }
+
+        public virtual User User { get; set; }
+        public virtual Venue Venue { get; set; }
+
+
+    }
+}
---
+++ CovidTrackingApp/CovidTrackingApp/Data/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CovidTrackingApp.Data
+{
+    public partial class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Postcode { get; set; }
+        public string PhoneNumber { get; set; }
+        public virtual ICollection<Booking> Booking { get; set; }
+
+        public User()
+        {
+            Booking = new HashSet<Booking>();
+        }
+    }
+}
