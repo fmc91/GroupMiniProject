52d880d2cd26872de7bc9b86e05fd86643f37651
Vinay Patel <vinay.light@hotmail.co.uk>
2020-10-28 14:25:39 +0000

    seedData

--- CovidTrackingApp/CovidTrackingApp/Startup.cs
+++ CovidTrackingApp/CovidTrackingApp/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using CovidTrackingApp.Pages.Home;
 
 namespace CovidTrackingApp
 {
@@ -25,7 +27,7 @@ namespace CovidTrackingApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddDbContext<CovidTrackerContext>();
+            services.AddDbContext<CovidTrackingContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
