c16baa8eeb2bedaa7912d6a8be67ec94899fffc8
Fazal Cheema <fazal.cheema@live.co.uk>
2020-10-28 14:25:43 +0000

    added page for venues

---
+++ CovidTrackingApp/CovidTrackingApp/Pages/Venues/Delete.cshtml.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CovidTrackingApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CovidTrackingApp.Pages.Venues
+{
+    public class DeleteModel : PageModel
+    {
+        private CovidTrackingContext _db;
+
+        public DeleteModel(CovidTrackingContext context)
+        {
+            _db = context;
+        }
+
+        public Venue SelectedVenue { get; set; }
+
+        public async Task<IActionResult> OnGet(int venueId)
+        {
+            SelectedVenue = await _db.Venue.FindAsync(venueId);
+
+            if (SelectedVenue == null)
+                return NotFound();
+
+            else
+                return Page();
+        }
+    }
+}
