8e5ef0f9b579cfa611cd7301659631aa3e05a569
Muhammed Miah <miahsuleman14@gmail.com>
2020-10-28 13:27:42 +0000

    Basic Formatting changes to views for User Also added a partial class extension in data

--- CovidTrackingApp/CovidTrackingApp/Data/User.cs
+++ CovidTrackingApp/CovidTrackingApp/Data/User.cs
@@ -10,13 +10,22 @@ namespace CovidTrackingApp.Data
     public partial class User
     {
         public int UserId { get; set; }
+
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
         public string Email { get; set; }
+
+        [Display(Name = "Date Of Birth")]
+        [DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
         public string Postcode { get; set; }
+
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
         public virtual ICollection<Booking> Booking { get; set; }
 
